072e957ee552e610724935ee3b871852a0083d54
EC2 Default User <ec2-user@ip-172-31-23-194.ec2.internal>
2022-02-28 01:54:12 +0000

    part1-2-3

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+        all_ratings = ["PG", "PG-13", "G", "R"]
+    end
+    
+    def self.with_ratings(ratings_priority)
+        Movie.where(:rating => ratings_priority)
+    end
 end
